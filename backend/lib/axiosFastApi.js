b638dc819da9c1bc7513174e4c3ddb08155b484e
Faiz Ahmad <faizahmad43430@gmail.com>
2026-01-24 19:44:06 +0530

    fixed

--- backend/lib/axiosFastApi.js
+++ backend/lib/axiosFastApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstanceFastApi = axios.create({
-  baseURL: process.env.AI_SERVICE_UR,
+  baseURL: process.env.AI_SERVICE_URL,
   timeout: 60000,
   withCredentials: true
 
