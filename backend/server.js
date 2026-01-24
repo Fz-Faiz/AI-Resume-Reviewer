b5b7911e71eb3c640ed6df6ef6a9a2e3a516feba
Faiz Ahmad <faizahmad43430@gmail.com>
2026-01-24 19:36:25 +0530

    server fixed 2

--- backend/server.js
+++ backend/server.js
@@ -31,16 +31,14 @@ app.use(cookieParser());
 app.use('/api/auth/', authRoutes);
 app.use('/api/resume/', resumeRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "frontend", "dist", "index.html")
-    );
-  });
-}
-
+// SPA fallback (Express v5 safe)
+app.use((req, res) => {
+  res.sendFile(
+    path.join(__dirname, "frontend", "dist", "index.html")
+  );
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server started at PORT: ${PORT}`);
